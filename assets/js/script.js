7d97eaf0b379a8b1d680d1f148afd37495cf460a
Arathi Variar <arathivariar@gmail.com>
2024-02-08 09:26:16 +0000

    Added the declaration of constants and variables in the JavaScript code

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+/* Declaring the constants and variables */
+const questionCount = document.querySelector(".number-of-question");
+const gameContainer = document.getElementById("container");
+const magicEffect = document.querySelectorAll(".magic-effect");
+const nextButton = document.getElementById("next-question-button");
+const playerScore = document.getElementById("player-score");
+const quizContainer = document.getElementById("quiz-container");
+const tryAgain = document.getElementById("try-again-button");
+const scoreContainer = document.querySelector(".score-container");
+const timeLeft = document.querySelector(".time-left");
+const beginButton = document.getElementById("begin-button");
+const welcomeScreen = document.querySelector(".welcome-screen");
+let count = 21;
+let countdown;
+let questionNumber;
+let scoreCount = 0;
