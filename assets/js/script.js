5d12ad5506e278ca2456743beb958e4ed90c4e8c
Arathi Variar <arathivariar@gmail.com>
2024-02-09 15:14:03 +0000

    Fixed various minor issues.Added a few changes for testing purposes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 /* Declaring the constants and variables */
 const questionCount = document.querySelector(".number-of-question");
-const gameContainer = document.getElementById("container");
+const gameContainer = document.getElementById("question-container");
 const magicEffect = document.querySelectorAll(".magic-effect");
 const nextButton = document.getElementById("next-question-button");
 const playerScore = document.getElementById("player-score");
@@ -9,7 +9,7 @@ const tryAgain = document.getElementById("try-again-button");
 const scoreContainer = document.querySelector(".score-container");
 const timeLeft = document.querySelector(".time-left");
 const beginButton = document.getElementById("begin-button");
-const gamesRulesButton = document.getElementById("game-rules-button");
+const gameRulesButton = document.getElementById("game-rules-button");
 const gameRulesText = document.getElementById("game-rules-text");
 const welcomeScreen = document.querySelector(".welcome-screen");
 let count = 21;
@@ -44,6 +44,19 @@ beginButton.addEventListener("click", function () {
     initial();
 });
 /**
+ * function to initially clear the quiz and restart
+ */
+function initial() {
+    gameContainer.innerHTML = "";
+    questionNumber = 0;
+    scoreCount = 0;
+    count = 21;
+    clearInterval(countdown);
+    timerDisplay();
+    quizGenerator();
+    quizDisplay(questionNumber);
+}
+/**
  * function to get a random question from the JSON file
  *  */ 
 function getRandomQuestions(questions) {
@@ -65,7 +78,7 @@ function timerDisplay() {
         timeLeft.innerHTML = `${count}s`;
         if (count == 0) {
             clearInterval(countdown);
-            displayNext();
+            displayNextQuestion();
         }
     }, 1500);
 }
@@ -92,13 +105,13 @@ function quizGenerator() {
         div.appendChild(question_DIV);
 
         div.innerHTML += `
-        <button class="option-div" onclick="${assertAnswer.name}(this)">
+        <button class="option-div" onclick="${checkAnswer.name}(this)">
         ${i.options[0]}</button>
-        <button class="option-div" onclick="${assertAnswer.name}(this)">
+        <button class="option-div" onclick="${checkAnswer.name}(this)">
         ${i.options[1]}</button>
-        <button class="option-div" onclick="${assertAnswer.name}(this)">
+        <button class="option-div" onclick="${checkAnswer.name}(this)">
         ${i.options[2]}</button>
-        <button class="option-div" onclick="${assertAnswer.name}(this)">
+        <button class="option-div" onclick="${checkAnswer.name}(this)">
         ${i.options[3]}</button>
         `;
 
@@ -163,6 +176,17 @@ function displayNextQuestion() {
 }
 
 nextButton.addEventListener("click", displayNextQuestion);
+/**
+ * function to display the quiz
+ */
+function quizDisplay(questionCount) {
+    let quizCards = document.querySelectorAll(".container-mid");
+
+    quizCards.forEach(function (card) {
+        card.classList.add("hide");
+    });
+    quizCards[questionCount].classList.remove("hide");
+}
 /**
  * function to display the game rules as a pop up text
  */
